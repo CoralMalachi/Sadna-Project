6a826bccd553b2a4d852d6c0889486ae693a6e78
Coral Malachi <38204874+CoralMalachi@users.noreply.github.com>
2018-12-15 17:17:44 +0200

    Won page - add return menu button

--- The Human Musicinator/src/View/WonPageController.java
+++ The Human Musicinator/src/View/WonPageController.java
@@ -1,4 +1,31 @@
 package View;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class WonPageController {
+    public void pressButtonPlayAgain(ActionEvent playAgainEvent){
+        System.out.print("move to play page");
+
+    }
+
+    public void pressButtonMenu(ActionEvent backMenuEvent){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("StartPage.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+            Main.stg.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        //System.out.print("Login");
+        //Pane root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    }
+
+
 }
