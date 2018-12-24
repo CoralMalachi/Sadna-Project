ce5bd28e8a86b8c70755c044ba69ee3bd95a87c3
CoralMalachi <314882853c@gmail.com>
2018-12-24 15:37:55 +0200

    Add more queries ** list of new table you need to download!

--- The Human Musicinator/src/Model/Queries.java
+++ The Human Musicinator/src/Model/Queries.java
@@ -3,6 +3,33 @@ package Model;
 import java.util.HashMap;
 import java.util.Map;
 
+//Todo: tables we need:
+/*
+    new tables :
+
+    release_group_primary_type
+    release_group
+    release
+    area_type
+    language
+    artist_credit
+    artist_credit_name
+    artist_alias
+
+    old:
+
+    area
+    artist
+    track
+
+    gender
+    recording - ?
+    l_artist_artist - ??
+
+
+
+ */
+
 /**
  * Created by user on 22/12/2018.
  */
@@ -18,11 +45,27 @@ public class Queries {
     //select the place which the artist born in
     public static final String bornPlaceQuery = "SELECT area.name FROM area INNER JOIN artist ON area.id=artist.area and artist.id =";
 
+    //Todo : Add more queries
+
     //select the gender of the artist
     public static final String genderQuery = "SELECT gender.name FROM gender INNER JOIN artist ON gender.id=artist.gender and artist.id =";
 
+    //select the type of the artist - Person / Group (band) / Choir / Orchestra / Character / Other
+    public static final String artistTypeQuery = "SELECT artist_type.name FROM artist_type INNER JOIN artist ON artist_type.id=artist.type and artist.id =";
+
+    //select the type of area the artist is born -Country / Island / City / ...
+    public static final String artistBornAreaTypeQuery = "SELECT area_type.name FROM area_type INNER JOIN area INNER JOIN artist ON area_type.id=area.type and area.id=artist.area and artist.id =";
 
 
+    //select random song of the artist - by level (easy/hard)
+
+    //select the language of song
+
+    //select random album of the artist - by level (easy/hard) - use Release group & release_group_primary_type (tell us if its a album/single
+
+    //number of albums artist has
+
+    //get artist_alias of the artist
 
 
 
