9fc8fca2fc548cbef304420509c07a3ef127d49f
CoralMalachi <314882853c@gmail.com>
2018-12-24 14:26:16 +0200

    Merge branch 'master' of /home/coral1219/IdeaProjects/Sadna-Project with conflicts.

--- The Human Musicinator/src/View/LoginController.java
+++ The Human Musicinator/src/View/LoginController.java
@@ -9,41 +9,9 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-/**
- * Created by limor on 18/12/2018.
- */
+
 
 public class LoginController {
-    @FXML
-    private TextField idNumberTextBox;
-
-    @FXML
-    public void pressButtonLogin(){
-        String idNumber = idNumberTextBox.getText();
-
-        if(!isValidIdNumber(idNumber)){
-            Alert wrongIdAlert = new Alert(AlertType.WARNING);
-            wrongIdAlert.setTitle("Wrong ID number");
-            wrongIdAlert.setHeaderText("Your Id number is not exist");
-            wrongIdAlert.setContentText("Please register before trying to login or try again");
-            wrongIdAlert.showAndWait();
-        }
-
-        if (isUserExistInDB(idNumber)) {
-            //Todo: save the user in DB and restart game
-            try {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ChooseGameDifficulty.fxml"));
-                Parent root = (Parent) fxmlLoader.load();
-                Stage stage = new Stage();
-                stage.setScene(new Scene(root));
-                stage.show();
-                Main.stg.close();
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     private boolean isValidIdNumber(String idNumber){
         return idNumber.matches("\\d{9}");
     }
@@ -51,6 +19,41 @@ public class LoginController {
         //Todo : check if user exist in DB and return the answer
         return true;//I wrote this only in order to avoid compilation errors
     }
+    @FXML
+    private TextField idNumberTextBox;
+
+    @FXML
+    public void pressButtonLogin()
+    {
+        String idNumber = idNumberTextBox.getText();
+
+        if(!isValidIdNumber(idNumber)){
+            Alert wrongIdAlert = new Alert(AlertType.WARNING);
+            wrongIdAlert.setTitle("Wrong ID number");
+        /**/    wrongIdAlert.setHeaderText("Your Id number is not exist");
+            wrongIdAlert.setContentText("Please register before trying to login or try again");
+            wrongIdAlert.showAndWait();
+        }
+
+        if (isUserExistInDB(idNumber)) {
+            //Todo: save the user in DB and restart game
+            try {
+
+                System.out.print("limor and coral");
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ChooseGameDifficulty.fxml"));
+                Parent root = (Parent) fxmlLoader.load();
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.show();
+                Main.stg.close();
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 
 }
 
