50042648c1735615df85e9c0094fc35ab1fc9e86
CoralMalachi <314882853c@gmail.com>
2018-12-29 11:31:08 +0200

    Add nested queries

--- The Human Musicinator/src/model/Queries.java
+++ The Human Musicinator/src/model/Queries.java
@@ -34,6 +34,16 @@ public class Queries {
     //select random id and name of an artist from artists table
     public static final String idQuery = "SELECT id, name FROM artist ORDER BY RAND() LIMIT 1";
 
+    //select random id and name of an artist from artists table - only from artists
+    //public static final String idOnlyArtistQuery = "SELECT artist.id,artist.name FROM artist where artist.type=1 ORDER BY RAND() LIMIT 1";
+
+    //NESTED QUERY - tested in mysql work - workbrench
+    public static final String idOnlyArtistQuery = "SELECT artist.id,artist.name FROM artist where artist.type=(select id from artist_type where artist_type.name=\"Person\") ORDER BY RAND() LIMIT 1";
+
+    //select random id and name of an artist from artists table - only from artists
+    public static final String idOnlyGroupQuery = "SELECT artist.id,artist.name FROM artist where artist.type=(select id from artist_type where artist_type.name=\"Group\") ORDER BY RAND() LIMIT 1";
+    //public static final String idOnlyGroupQuery = "SELECT artist.id,artist.name FROM artist where artist.type=2 ORDER BY RAND() LIMIT 1";
+
     //select the date of birth of an artist according to it's id
     public static final String yearQuery = "select begin_date_year from artist where id = ?";
     public static final String monthQuery = "select begin_date_month from artist where id = ?";
@@ -58,7 +68,13 @@ public class Queries {
 
     //select the language of song
 
+    //select a random album - nested query
+    public static final String albumRandomQuery = "select `release`.name from `release` INNER JOIN release_group ON release_group.id=`release`.release_group and release_group.type=(select id from release_group_primary_type where release_group_primary_type.name=\"Album\") ORDER BY RAND() LIMIT 1";
+
     //select random album of the artist - by level (easy/hard) - use Release group & release_group_primary_type (tell us if its a album/single
+
+    //select random album by artist and language TODO:add input artist name
+    public static final String albumRandomOfArtistQuery = "select `release`.id,`release`.name from `release` INNER JOIN release_group ON release_group.id=`release`.release_group and release_group.type=(select id from release_group_primary_type where release_group_primary_type.name=\"Album\") where `release`.language=(select id from language where language.name=\"English\") and `release`.artist_credit=(select id from artist_credit where artist_credit.name=\"[Disney]\" limit 1) ORDER BY RAND() LIMIT 1";
 
     //get artist_alias of the artist
 
